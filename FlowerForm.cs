0543e343bdd350ba36b928bac61ddf99ce433e11
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2019-11-20 16:36:55 -0800

    Pretty much done
    ∆t is being incremented by too large of a number. Ideally, the distance from one dot to the next should be the dot's radius. I believe that by using information from the below websites, we can solve for ∆t.
    https://web.ma.utexas.edu/users/m408s/m408d/CurrentWeb/LM10-4-4.php
    http://tutorial.math.lamar.edu/Classes/CalcII/PolarArcLength.aspx
    
    Also, when t is greater than or equal to 2π, the clocks should stop.
    
    And maybe draw some axes too.

--- FlowerForm.cs
+++ FlowerForm.cs
@@ -14,7 +14,7 @@
 //  1. FlowerForm.cs compiles into library file FlowerForm.dll
 //  2. FlowerMain.cs compiles and links with the dll file above to create Flower.exe
 // Compile (and link) this file:
-// mcs
+// mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -out:FlowerForm.dll FlowerForm.cs
 
 using System;
 using System.Windows.Forms;
@@ -23,19 +23,23 @@ using System.Timers;
 
 public class FlowerForm : Form
 {
-  private const short formWidth = 1600;
-  private const short formHeight = formWidth*9/16;
-  private const float refreshRate = 1000/20;   //20 frames per second
-  private const float animationRate = 1000/20; //20 circles per second
-
+  // Create Controls
   private Label title = new Label();
   private Button startButton = new Button();
   private Label coordinates = new Label();
   private Button pauseButton = new Button();
   private Button exitButton = new Button();
 
+  // Create Timers
+  private const float refreshRate = 1000/4; //4 frames per second
+  private const float animationRate = 1000/20; //20 dots per second
   private System.Timers.Timer refreshClock = new System.Timers.Timer(refreshRate);
   private System.Timers.Timer animationClock = new System.Timers.Timer(animationRate);
+
+  // These will be used in drawing the dots
+  private const short radius = 4;
+  private double t = 0; //used in calculating the coordinates of each dot
+  private PointF myPoint;
 
   private System.Drawing.Bitmap pointer_to_bitmap;
   private System.Drawing.Graphics pointer_to_graphics;
@@ -45,24 +49,26 @@ public class FlowerForm : Form
     // Set up texts
     Text = "Flower";
     title.Text = "Flower by Anthony Sam";
+    title.TextAlign = ContentAlignment.MiddleCenter;
     startButton.Text = "Go";
     coordinates.Text = "X:\nY:";
     pauseButton.Text = "Pause";
     exitButton.Text = "Exit";
 
     // Set up sizes
-    Size = new Size(formWidth, formHeight);
-    title.Size = new Size(formWidth, formHeight/20);
+    Width = 1600;
+    Height = Width*9/16;
+    title.Size = new Size(Width, Height/20);
     coordinates.AutoSize = true;
 
     // Set up locations
-    startButton.Location = new Point(formWidth/5, formHeight*19/20);
-    coordinates.Location = new Point(formWidth*2/5, formHeight*19/20);
-    pauseButton.Location = new Point(formWidth*3/5, startButton.Top);
-    exitButton.Location = new Point(formWidth*4/5, startButton.Top);
+    startButton.Location = new Point(Width/5, Height*19/20-startButton.Height/2);
+    coordinates.Location = new Point(Width*2/5, Height*19/20-coordinates.Height/2);
+    pauseButton.Location = new Point(Width*3/5, startButton.Top);
+    exitButton.Location = new Point(Width*4/5, startButton.Top);
 
     // Set up colors
-    BackColor = Color.Orange;
+    BackColor = Color.Magenta;
     title.BackColor = Color.Cyan;
 
     // Add Controls to the form
@@ -72,14 +78,53 @@ public class FlowerForm : Form
     Controls.Add(pauseButton);
     Controls.Add(exitButton);
 
-    pointer_to_bitmap = new Bitmap(1200, 700, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+    // Define how methods are called
+    startButton.Click += new EventHandler(start);
+    pauseButton.Click += new EventHandler(pause);
+    exitButton.Click += new EventHandler(exit);
+    refreshClock.Elapsed += new ElapsedEventHandler(refresh);
+    animationClock.Elapsed += new ElapsedEventHandler(update);
+
+    pointer_to_bitmap = new Bitmap(Width, Height*9/10, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
     pointer_to_graphics = Graphics.FromImage(pointer_to_bitmap);
   }
 
   protected override void OnPaint(PaintEventArgs e)
   {
     Graphics graphics = e.Graphics;
-    graphics.FillRectangle(Brushes.Yellow, 0, formHeight*9/10, formWidth, formHeight/10);
+    graphics.FillRectangle(Brushes.LawnGreen, 0, Height*9/10, Width, Height/10);
+    graphics.DrawImage(pointer_to_bitmap, 0, title.Height, Width, Height*9/10-title.Height); //copy from bitmap to graphic surface
     base.OnPaint(e);
+  }
+
+  protected void start(Object sender, EventArgs e)
+  {
+    refreshClock.Start();
+    animationClock.Start();
+  }
+
+  protected void pause(Object sender, EventArgs e)
+  {
+    refreshClock.Stop();
+    animationClock.Stop();
+  }
+
+  protected void exit(Object sender, EventArgs e)
+  {
+    System.Console.WriteLine("You clicked on the Exit button.");
+    Close();
+  }
+
+  protected void refresh(Object sender, ElapsedEventArgs events)
+  {
+    Invalidate();
+  }
+
+  protected void update(Object sender, ElapsedEventArgs events)
+  {
+    // Draw on bitmap
+    pointer_to_graphics.FillEllipse(Brushes.DarkOrchid, (float)(Math.Cos(2*t)*Math.Cos(t)*startButton.Width+Width/2), (float)(Math.Cos(2*t)*Math.Sin(t)*startButton.Width+Height/2), 4, 4);
+
+    t += Math.PI/24; //increment t
   }
 }
